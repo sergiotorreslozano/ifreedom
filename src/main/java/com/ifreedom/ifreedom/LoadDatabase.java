cd240473bf04a4c5fcf769318d21a8cb84f79140
Sergio Torres Lozano <sergiotorreslozano@gmail.com>
2023-08-24 17:36:46 +0200

    Added log configuration

--- src/main/java/com/ifreedom/ifreedom/LoadDatabase.java
+++ src/main/java/com/ifreedom/ifreedom/LoadDatabase.java
@@ -3,6 +3,8 @@ package com.ifreedom.ifreedom;
 import com.ifreedom.ifreedom.stocks.OperationRepository;
 import com.ifreedom.ifreedom.stocks.model.Operation;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,9 +19,11 @@ public class LoadDatabase {
     @Autowired
     private OperationRepository operationRepository;
 
+    Logger logger = LoggerFactory.getLogger(LoadDatabase.class);
+
     @PostConstruct
     public void init() {
-        System.out.println("Initializing mock operation list");
+        logger.debug("Initializing mock operation list");
         List operations = new ArrayList<>();
         Operation operation = new Operation("AAPL", new BigDecimal(100L),
                 10L, LocalDate.parse("2023-08-12").atStartOfDay());
